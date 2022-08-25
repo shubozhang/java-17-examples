a87511a6808d3493ca88d988cbe881745dcb47de
shubo zhang <szhang@tenable.com>
2022-08-25 11:49:47 -0400

    added misc module

---
+++ examples/misc/src/main/java/misc/App.java
@@ -0,0 +1,12 @@
+package misc;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class App {
+    public static void main(String[] args) {
+        String num = "01234";
+
+        System.out.println(NumberUtils.isCreatable(num));
+    }
+
+}
